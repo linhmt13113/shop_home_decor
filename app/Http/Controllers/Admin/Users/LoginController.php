6c8b3faf228a0c1996492ac3889477bdb8196fbc
My name Ngoc Linh <105900871+linhmt13113@users.noreply.github.com>
2024-12-03 11:03:10 +0700

    init project

---
+++ app/Http/Controllers/Admin/Users/LoginController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Users;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        return view('admin.users.login', [
+            'title' => 'Login System'
+        ]);
+    }
+
+    use \Illuminate\Foundation\Validation\ValidatesRequests;
+    public function store(Request $request)
+    {
+        // dd($request->input());
+
+        $this->validate($request, [
+            'email' => 'required|email:filter',
+            'password' => 'required',
+        ]);
+
+        if (
+            Auth::attempt([
+                'email' => $request->input('email')
+                ,
+                'password' => $request->input('password'),
+            ], $request->input('remember'))
+        ) {
+            return redirect()->route('admin');
+        }
+        return redirect()->back();
+
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\Admin\Users\LoginController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\MainController;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('admin/users/login', [LoginController::class, 'index']);
+Route::post('admin/users/login/store', [LoginController::class, 'store']);
+
+
+
+
+Route::get('admin/main',[MainController::class, 'index'])->name('admin');
