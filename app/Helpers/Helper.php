647e88e0fdea38928e3561341f7561937e6a9dc3
My name Ngoc Linh <105900871+linhmt13113@users.noreply.github.com>
2024-12-05 19:18:02 +0700

    end menu_category

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -17,7 +17,7 @@ class Helper
                     <tr>
                         <td>' . $menu->id . '</td>
                         <td>' . $char . $menu->name . '</td>
-                        <td>' . $menu->active . '</td>
+                        <td>' . self::active($menu->active) . '</td>
                         <td>' . $menu->updated_at . '</td>
                         <td>
                             <a class="btn btn-primary btn-sm" href="/admin/menus/edit/' . $menu->id . '">
@@ -39,4 +39,12 @@ class Helper
 
         return $html;
     }
+
+    public static function active($active = 0): string
+    {
+        return $active == 0
+            ? '<span class="btn btn-danger btn-xs rounded-pill"><i class="fas fa-times-circle"></i> NO</span>'
+            : '<span class="btn btn-success btn-xs rounded-pill"><i class="fas fa-check-circle"></i> YES</span>';
+    }
+
 }
